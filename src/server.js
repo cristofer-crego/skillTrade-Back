a0e67bc3badd8c21cb6a4c7fb3e883ab6c905a2f
cristofer.crego <cregocrisoferlf@gmail.com>
2024-09-16 08:35:10 -0300

    resolving error cors 3

--- src/server.js
+++ src/server.js
@@ -13,30 +13,23 @@ const routeProfessions = require("./routes/routeProfessions");
 server.use(morgan("dev"));
 server.use(express.json());
 server.use(cookieParser());
-// const corsOptions = {
-//   origin: "https://skill-trade-front.vercel.app",
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// };
-// server.options("*", cors(corsOptions));
 
-// server.use(cors(corsOptions));
-server.use(
-  cors({
-    origin: [
-      "https://skill-trade-front.vercel.app",
-      "https://skilltrade-back.onrender.com",
-    ],
-    methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "auth", "Authorization"],
-    credentials: true,
-  })
-);
-// Manejar errores 404 para rutas no encontradas
-server.use((req, res) => {
-  res.status(404).send("Not Found");
+//CORS con opciones específicas
+// server.use(
+//   cors({
+//     origin: "https://skilltrade-back.onrender.com", //cambiaremos esto al dominio del cliente al deployar
+//     credentials: true, //esto habilita el envío de cookies en solicitudes CORS
+//   })
+// );
+const corsOptions = {
+  origin: "https://skill-trade-front.vercel.app", // Permitir solicitudes desde este dominio
+  optionsSuccessStatus: 200,
+};
+
+server.use(cors(corsOptions));
+server.get("/", (req, res) => {
+  res.send("Bienvenido a la API");
 });
-
 //rutas
 server.use("/user", routeUser);
 server.use("/professions", routeProfessions);
