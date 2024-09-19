e65d917923d9106f79b0c0854ca81bda0dec3972
cristofer.crego <cregocrisoferlf@gmail.com>
2024-09-18 22:04:55 -0300

    token

--- src/controllers/loginUser.js
+++ src/controllers/loginUser.js
@@ -1,40 +1,45 @@
-const { Users } = require('../db')
-const bcrypt = require ('bcrypt')
-const jwt = require ('jsonwebtoken')
-const SECRET_KEY = process.env.JWT_SECRET_KEY
+const { Users } = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = process.env.JWT_SECRET_KEY;
 
-const loginUser = async (req, res) =>{
-    try{
-        let { email, password} = req.body
+const loginUser = async (req, res) => {
+  try {
+    let { email, password } = req.body;
 
-        const existingUser = await Users.findOne({ where: { email } });
+    const existingUser = await Users.findOne({ where: { email } });
 
-        if (!existingUser) {
-            return res.status(404).json({ error: "Email no encontrado" });
-        }
-
-        const isValid = bcrypt.compareSync(password, existingUser.password);
-
-        if (isValid){
-            const token = jwt.sign({ id: existingUser.id, email: existingUser.email}, SECRET_KEY, {expiresIn: '1h'})
-
-            res.cookie('token', token, {
-                httpOnly: false,  //solo accesible a través de HTTP
-                secure: process.env.NODE_ENV === 'production', // solo se envía a través de HTTPS en producción
-                sameSite: 'Strict',  //protección contra CSRF
-                maxAge: 3600000,
-              });
-              const { password, ...userWithoutPassword } = existingUser.dataValues;
-              return res.status(200).json({ message: 'login realizado', user: userWithoutPassword });
-        }
-        else{
-            return res.status(401).json({error: 'Contraseña incorrecta'});
-        }
-    } catch (error){
-        console.error(error);
-        return res.status(500).json({error: 'Error de servidor'})
+    if (!existingUser) {
+      return res.status(404).json({ error: "Email no encontrado" });
     }
-}
+
+    const isValid = bcrypt.compareSync(password, existingUser.password);
+
+    if (isValid) {
+      const token = jwt.sign(
+        { id: existingUser.id, email: existingUser.email },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+
+      res.cookie("token", token, {
+        httpOnly: false, //solo accesible a través de HTTP
+        secure: process.env.NODE_ENV === "production", // solo se envía a través de HTTPS en producción
+        sameSite: process.env.NODE_ENV === "production" ? "Strict" : "Lax", //protección contra CSRF Strict
+        maxAge: 3600000,
+      });
+      const { password, ...userWithoutPassword } = existingUser.dataValues;
+      return res
+        .status(200)
+        .json({ message: "login realizado", user: userWithoutPassword });
+    } else {
+      return res.status(401).json({ error: "Contraseña incorrecta" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Error de servidor" });
+  }
+};
 
 module.exports = loginUser;
 
